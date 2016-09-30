43146270e555dc95fe34061d9a419b4f38444dab
Narveid <holjoa13@gmail.com>
2016-10-01 01:36:10 +0200

    no message

---
+++ Assets/Scripts/Menu/fadeOutPiano.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeOutPiano : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<AudioSource>().volume += -0.0015f;
+	
+	}
+}
