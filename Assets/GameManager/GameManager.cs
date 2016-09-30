907edd36bde66d35a0f448c404e0af18a4297503
tromat14 <trmat14@student.westerdals.no>
2016-09-30 23:15:16 +0200

    Reuploaded'

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         DontDestroyOnLoad(gameObject);
-	}
+    }
+
+
+    // Doubles as reset function by
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("The Scene was changed to " + sceneName);
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("The Scene was reset to: " + SceneManager.GetActiveScene().name);
+    }
 }
