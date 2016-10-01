a1508a928fa0e52b4e1e97ba625d0313a202b7a2
tromat14 <trmat14@student.westerdals.no>
2016-10-01 02:43:24 +0200

    Added swisj background music

---
+++ Assets/LevelAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelAudio : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().name != "menu")
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+}
